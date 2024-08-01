b42eb545513e6a1c925483344797dcd67877eebd
AlexeyZh <waltafunk@gmail.com>
2024-08-02 02:07:51 +0300

    style: format code with ktlint

--- src/test/kotlin/com/petstore/model/Pet.kt
+++ src/test/kotlin/com/petstore/model/Pet.kt
@@ -1,3 +1,10 @@
 package com.petstore.model
 
-data class Pet(val id: Long, val category: Category, val name: String, val photoUrls: List<String>, val tags: List<Tag>, val status: String)
+data class Pet(
+    val id: Long,
+    val category: Category,
+    val name: String,
+    val photoUrls: List<String>,
+    val tags: List<Tag>,
+    val status: String,
+)
